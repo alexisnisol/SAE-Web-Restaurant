0076ec5de7a430a3f4d730289e8cc0b136622b32
Zouadi Mouad <o22304981@or-iut-i203-f06.iut45.univ-orleans.fr>
2025-03-03 16:47:47 +0100

    etoiles obligatoire pour pouvoir mettre un avis et style de la page profil

--- src/templates/auth/profil.php
+++ src/templates/auth/profil.php
@@ -1,10 +1,22 @@
-<div class="container">
-        <h2>Modifier le Profil</h2>
+<div class="page">
+    <div class="form-container">
+        <h2>Modifier mon profil</h2>
+        
         <form action="#" method="POST">
-            <input type="text" name="nom" placeholder="Nom" required>
-            <input type="text" name="prenom" placeholder="Prénom" required>
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="mdp" placeholder="Nouveau mot de passe" required>
-            <button type="submit">Enregistrer</button>
+            <input type="text" name="firstName" placeholder="Prénom" value="<?=$user->firstName ?>" required>
+            <input type="text" name="lastName" placeholder="Nom" value="<?=$user->lastName?>" required>
+            <input type="email" name="email" placeholder="Adresse mail" value="<?=$user->email?>" required>
+            <input type="password" name="password" placeholder="Mot de passe">
+            
+            <?php 
+            if (isset($error)) {
+                echo '<p class="error-message">*' . $error . '</p>';
+            }
+            ?>
+            
+            <button type="submit">Modifier</button>
         </form>
+
+        <a href="./index.php?action=planning" class="register-link">Retour</a>
     </div>
+</div>
