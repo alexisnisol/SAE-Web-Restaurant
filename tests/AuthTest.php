94f6dc1979ce63c1e094a4715cb6ad485ffb5272
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2025-03-05 11:29:31 +0100

    AuthTest Valide

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -5,19 +5,24 @@ namespace Tests;
 use PHPUnit\Framework\TestCase;
 use App\Controllers\Auth\Auth;
 use App\Controllers\Auth\Users\User;
+use App\Controllers\Auth\Users\Role;
 
 class AuthTest extends TestCase {
-    public function testSetUserSession()
-    {
+    public function testSetUserSession() {
         $user = new User(1, 'John', 'Doe', 'john@example.com', 'password', 'CLIENT');
         Auth::setUserSession($user);
         $this->assertTrue(isset($_SESSION['user']));
     }
 
-    public function testIsUserLoggedIn()
-    {
+    public function testIsUserLoggedIn() {
         $_SESSION['user'] = serialize(new User(1, 'John', 'Doe', 'john@example.com', 'password', 'CLIENT'));
         $this->assertTrue(Auth::isUserLoggedIn());
+    }
+
+    public function testGetCurrentUser() {
+        $_SESSION['user'] = serialize(new User(1, 'John', 'Doe', 'john@example.com', 'password', 'CLIENT'));
+        $user = Auth::getCurrentUser();
+        $this->assertInstanceOf(User::class, $user);
     }
 }
 
