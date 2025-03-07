e1ff3e46a77b946dc8d9a7e93be7e916f3bc8d67
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2025-03-07 08:06:09 +0100

    DatabaseTest Valide

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -28,49 +28,14 @@ class DatabaseTest extends TestCase {
         $pdo = $db->getPDO();
         $this->assertNotNull($pdo);
     }
-
-    // public function testGetDBWithConfig() {
-    //     $db = Database::getDBWithConfig('mysql');
-    //     $this->assertNotNull($db);
-    // }
-
-    // public function testGetPDOWithConfig() {
-    //     $pdo = Database::getPDOWithConfig('mysql');
-    //     $this->assertNotNull($pdo);
-    // }
-
-    // public function testGetDBWithConfigSQLite() {
-    //     $db = Database::getDBWithConfig('sqlite');
-    //     $this->assertNotNull($db);
-    // }
-
-    // public function testGetPDOWithConfigSQLite() {
-    //     $pdo = Database::getPDOWithConfig('sqlite');
-    //     $this->assertNotNull($pdo);
-    // }
-
-    // public function testGetDBWithConfigUnknown() {
-    //     $db = Database::getDBWithConfig('unknown');
-    //     $this->assertNull($db);
-    // }
-
-    // public function testGetPDOWithConfigUnknown() {
-    //     $pdo = Database::getPDOWithConfig('unknown');
-    //     $this->assertNull($pdo);
-    // }
-
-    // public function testDatabaseConnection()
-    // {
-    //     $db = new SQLiteDatabase('src/static/data/database.db');
-    //     $this->assertNotNull($db->getPDO());
-    // }
     
-    // public function testDatabaseQuery()
-    // {
-    //     $db = new SQLiteDatabase('src/static/data/database.db');
-    //     $result = $db->query('SELECT 1');
-    //     $this->assertNotFalse($result);
-    // }
+    public function testDatabaseQuery()
+    {
+        // $db = new SQLiteDatabase('src/static/data/database.db');
+        $db = App::getApp()->getDB();
+        $result = $db->query('SELECT 1');
+        $this->assertNotFalse($result);
+    }
 }
 
 ?>
